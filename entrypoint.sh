668d4dfaa186cb66160e69caefdd9c12cd0c332a
KIMJiho <rlawlgh1028@naver.com>
2024-04-01 20:13:28 +0900

    #2 Optimized deployment

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
-echo "python3 manage.py makemigrations"
-python3 manage.py makemigrations
-echo "python3 manage.py migrate"
-python3 manage.py migrate
-echo "gunicorn -b 0.0.0.0:8000 config.wsgi:application"
+echo "python3 manage.py makemigrations" && 
+python manage.py makemigrations &&
+echo "python3 manage.py migrate" && 
+python manage.py migrate --no-input &&
+echo "gunicorn -b 0.0.0.0:8000 config.wsgi:application" &&
 gunicorn -b 0.0.0.0:8000 config.wsgi:application
