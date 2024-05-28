2e2b93a3236e78d186c96df27e94c54ca1d8988c
KIMJiho <rlawlgh1028@naver.com>
2024-05-28 22:32:56 +0900

    fix cd script

--- etc/startup.sh
+++ etc/startup.sh
@@ -2,10 +2,10 @@ echo compose stop
 docker compose stop
 
 echo delete was
-docker rm was
+docker rm -f was
 
 echo pull container
 docker pull rlawlgh001028/bigsasimi-django:latest
 
 echo docker compose up -d
-docker docker compose up -d
+docker compose up -d
