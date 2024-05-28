45cee49a60ec75a4c8a71f5f4bbecf443802b0fa
KIMJiho <rlawlgh1028@naver.com>
2024-05-28 22:01:34 +0900

    add cd

---
+++ etc/startup.sh
@@ -0,0 +1,11 @@
+echo compose stop
+docker compose stop
+
+echo delete was
+docker rm was
+
+echo pull container
+docker pull rlawlgh001028/bigsasimi-django:latest
+
+echo docker compose up -d
+docker docker compose up -d
